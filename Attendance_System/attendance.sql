4e3f7c4d2b680742c2d516272826a3f35a14d5ee
Yashvigor <yashvigor.ce@gmail.com>
2025-09-16 21:39:02 +0530

    Update attendance.sql

--- Attendance_System/attendance.sql
+++ Attendance_System/attendance.sql
@@ -1,32 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.0.4
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: May 12, 2014 at 05:39 PM
--- Server version: 5.6.12-log
--- PHP Version: 5.4.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `attendance`
---
 CREATE DATABASE IF NOT EXISTS `attendance` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `attendance`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `objects`
---
 
 CREATE TABLE IF NOT EXISTS `objects` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
@@ -39,12 +14,6 @@ CREATE TABLE IF NOT EXISTS `objects` (
   UNIQUE KEY `uid` (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `teacher`
---
-
 CREATE TABLE IF NOT EXISTS `teacher` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(25) NOT NULL,
@@ -54,6 +23,4 @@ CREATE TABLE IF NOT EXISTS `teacher` (
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
